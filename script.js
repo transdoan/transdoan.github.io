da5aa2e44afba4268493bc98c40c6da3d28f4155
Tran Doan <transdoan02@gmail.com>
2025-09-24 15:00:04 -0400

    idk

---
+++ script.js
@@ -0,0 +1,20 @@
+function copyContent() {
+    const email = "tran.doan@yale.edu";
+    const button = document.getElementById('email-link-button');
+    
+    navigator.clipboard.writeText(email)
+        .then(() => {
+            const originalText = button.innerHTML;
+            button.innerHTML = 'copied to clipboard!';
+            button.style.color = '#e8706c'; 
+            
+            // rvert 'copied!' text after 1.5 seconds
+            setTimeout(() => {
+                button.innerHTML = originalText;
+                button.style.color = ''; // back to og color
+            }, 1500);
+        })
+        .catch(err => {
+            console.error('Failed to copy text!!!!: ', err);
+        });
+}
